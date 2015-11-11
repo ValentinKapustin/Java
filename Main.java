0fc37643d064218318b9af8276a6e096a55123b0
ValentinKapustin <kapustin1211@rambler.ru>
2015-11-11 19:30:34 +0900

    Update Main.java

--- Main.java
+++ Main.java
@@ -6,11 +6,11 @@ public class Main {
 		Fraction a=new Fraction(0,0,"");
 		Fraction b=new Fraction(0,0,"");
 				
-		a.n = 3; a.d=6; a.name="Äðîáü 1";
-		b.n = 1; b.d=2; b.name="Äðîáü 2";
+		a.n = 3; a.d=6; a.name="Дробь 1";
+		b.n = 1; b.d=2; b.name="Дробь 2";
 		
-		//a.n = 3; a.d=6; a.name="Äðîáü 1";
-		//b.n = 8; b.d=4; b.name="Äðîáü 2";
+		//a.n = 3; a.d=6; a.name="Дробь 1";
+		//b.n = 8; b.d=4; b.name="Дробь 2";
 				
 		System.out.println(a);
 		System.out.println(b);
